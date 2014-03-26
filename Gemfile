17d4cf082ae507197341c7c262f36ed9291db6a4
Andrew House <andrewhouse621@gmail.com>
2014-03-26 14:30:16 -0400

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -2,13 +2,13 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.4'
-
 gem 'bootstrap-sass'
 gem 'sprockets'
 gem 'bcrypt-ruby'
 gem 'faker'
 gem 'will_paginate'
 gem 'bootstrap-will_paginate'
+
 # Use sqlite3 as the database for Active Record
 group :development, :test do
 	gem 'sqlite3'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
 		@user = User.new
-	end
-
-	def show
-		@user = User.find(params[:id])
 	end
 
 	def create
@@ -55,16 +52,6 @@ class UsersController < ApplicationController
     	params.require(:user).permit(:name, :email, :password,
     		:password_confirmation)
     end
-
-   # Before filters
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
 
     def correct_user
       @user = User.find(params[:id])
